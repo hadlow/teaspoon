da6c8c726953043e24e0163204db3e660a4c27d4
Billy Hadlow <me@billyhadlow.com>
2021-01-26 07:49:36 +0000

    Working network

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import random
 import math
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
 import pandas as pd
 import matplotlib.pyplot as plt
+from python_tsp.distances import euclidean_distance_matrix
+
+from encoder_decoder import decode
 
 n_locations = 10
-batch_size = 30
-epochs = 5
+batch_size = 64
+train_examples = 20
+epochs = 400
 losses = []
 
 class Model(nn.Module):
@@ -22,7 +28,7 @@ class Model(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.output(x)
+        x = torch.sigmoid(self.output(x))
 
         return x
 
@@ -31,30 +37,71 @@ model = Model(n_locations)
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
 criterion = nn.MSELoss()
 
-x_train = torch.FloatTensor(pd.read_csv('./data/x_train.csv').values)
-x_test = torch.FloatTensor(pd.read_csv('./data/x_test.csv').values)
+x = pd.read_csv('./data/x_raw.csv')
+y = pd.read_csv('./data/y_encoded.csv')
 
-y_train_raw = pd.read_csv('./data/y_train.csv')
-y_train = torch.FloatTensor(y_train_raw.drop('11', axis = 1).values)
-y_train_tour_lengths = y_train_raw['11'].values
+train_dataset = TensorDataset(torch.FloatTensor(x.values[:train_examples]), torch.FloatTensor(np.delete(y.values[:train_examples], 10, axis=1)))
+test_dataset = TensorDataset(torch.FloatTensor(x.values[train_examples:]), torch.FloatTensor(np.delete(y.values[train_examples:], 10, axis=1)))
 
-y_test_raw = pd.read_csv('./data/y_test.csv')
-y_test = torch.FloatTensor(y_test_raw.drop('11', axis = 1).values)
-y_test_tour_lengths = y_test_raw['11'].values
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 for epoch in range(epochs):
-    y_pred = model.forward(x_train)
+    for batch in train_loader:
+        x_train = batch[0]
+        y_train = batch[1] 
 
-    loss = criterion(y_pred, y_train)
+        y_pred = model.forward(x_train)
 
-    losses.append(loss)
+        loss = criterion(y_pred, y_train)
+
+        losses.append(loss)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
     print(f'Epoch {epoch}: {loss}')
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
 
 plt.plot(range(epochs), losses)
 plt.xlabel('Loss')
 plt.ylabel('Epoch')
+plt.show()
+
+def draw_edge(a, b):
+    x = [a[0], b[0]]
+    y = [a[1], b[1]]
+
+    plt.plot(x, y, 'k-')
+
+def draw_route(permutation, points):
+    for position_index, location_index in enumerate(permutation):
+        if len(permutation) == position_index + 1:
+            next_location_index = permutation[0]
+        else:
+            next_location_index = permutation[position_index + 1]
+
+        location_from = points[location_index]
+        location_to = points[next_location_index]
+        
+        draw_edge(location_from, location_to)
+
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
+
+for index, test_batch in enumerate(test_loader):
+    if index > 0:
+        break
+
+    random_sample = random.randint(0, 10)
+    test_x = batch[0][random_sample]
+
+    pred = model.forward(test_x)
+    decoded_pred = decode(pred.detach().numpy()).astype('int')
+
+    test_x = test_x.detach().numpy().reshape(-1, 2)
+
+    draw_route(decoded_pred, test_x)
+
